86cc296e1c9161dc01495ef77e7c83c0f6677334
Stephan <stephan.kop@outlook.com>
2019-01-19 17:58:20 +0100

    started on heatmap

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 
 
-var value = document.getElementById('slider').value;
+// var value = document.getElementById('slidermap').value;
+// slider.addEventListener('input', sliderChange);
+
+
+// function sliderChange() {
+    
+//     if (value = "11") {
+//         document.getElementById("chart1").style.display = "none";
+//         document.getElementById("chart2").style.removeProperty("display");
+//         document.getElementById("VSmap1").style.display = "none";
+//         document.getElementById("VSmap2").style.removeProperty("display");
+//     }
+//     else if (value = '21') {
+//         document.getElementById("chart2").style.display = "none";
+//     }
+
+    
+//   console.log(value);
+// }
+
+var value = document.getElementById('slidermap').value;
 slider.addEventListener('input', sliderChange);
 
 
 function sliderChange() {
     
     if (value = "11") {
-        document.getElementById("chart1").style.display = "none";
-        document.getElementById("chart2").style.removeProperty("display");
-        document.getElementById("VSmap1").style.display = "none";
-        document.getElementById("VSmap2").style.removeProperty("display");
+        document.getElementById("VS").style.display = "none";
+        document.getElementById("VS1").style.removeProperty("display");
+        document.getElementById("NL").style.display = "none";
+        document.getElementById("NL1").style.removeProperty("display");
+        document.getElementById("date").innerHTML = "1 Juli 2016"
     }
     else if (value = '21') {
         document.getElementById("chart2").style.display = "none";
