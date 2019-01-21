8808fe8133bab1c72d4a353336b369991729c5e7
Stephan <stephan.kop@outlook.com>
2019-01-21 13:08:56 +0100

    slider werkend

--- js/main.js
+++ js/main.js
@@ -20,23 +20,40 @@
 //   console.log(value);
 // }
 
-var value = document.getElementById('slidermap').value;
-slider.addEventListener('input', sliderChange);
+// var x = document.getElementById('slidermap').value;
+document.getElementById("slidermap").addEventListener('input', sliderChange);
 
 
 function sliderChange() {
+
+    var x = document.getElementById('slidermap').value;
     
-    if (value = "11") {
+    if (x <= "10") {
         document.getElementById("VS").style.display = "none";
         document.getElementById("VS1").style.removeProperty("display");
+        document.getElementById("VS2").style.display ="none";
         document.getElementById("NL").style.display = "none";
         document.getElementById("NL1").style.removeProperty("display");
+        document.getElementById("NL2").style.display ="none";
+        document.getElementById("UK").style.display = "none";
+        document.getElementById("UK1").style.removeProperty("display");
+        document.getElementById("UK2").style.display ="none";
+        document.getElementById("DE").style.display = "none";
+        document.getElementById("DE1").style.removeProperty("display");
+        document.getElementById("DE2").style.display ="none";
         document.getElementById("date").innerHTML = "1 Juli 2016"
     }
-    else if (value = '21') {
-        document.getElementById("chart2").style.display = "none";
+    else if (x <= "20") {
+        document.getElementById("VS1").style.display = "none";
+        document.getElementById("VS2").style.removeProperty("display");
+        document.getElementById("NL1").style.display = "none";
+        document.getElementById("NL2").style.removeProperty("display");
+        document.getElementById("UK1").style.display = "none";
+        document.getElementById("UK2").style.removeProperty("display");
+        document.getElementById("DE1").style.display = "none";
+        document.getElementById("DE2").style.removeProperty("display");
+        document.getElementById("date").innerHTML = "1 Oktober 2016"
     }
 
-    
-  console.log(value);
+  console.log(x);
 }
